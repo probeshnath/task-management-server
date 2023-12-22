fc88fecb75cb949bc257a4486a53df032650deab
probeshnath <probeshn@gmail.com>
2023-12-22 17:41:48 +0600

    vercel added

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ async function run() {
 
 
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
